4663b982057416fdc67767b46dfeb2af7299a826
Victor Chuyen <167945697+tncsharetool@users.noreply.github.com>
2025-04-04 16:04:13 +0700

    Update firebase_config.py

--- core/firebase_config.py
+++ core/firebase_config.py
@@ -1,14 +1,15 @@
 import pyrebase
+import os
 
 firebase_config = {
-    "apiKey": "AIzaSyBGZe rE...gN6g4Lc",  # bạn copy key từ Firebase
-    "authDomain": "vietnam-ai-travel.firebaseapp.com",
-    "databaseURL": "",  # nếu dùng realtime database
-    "projectId": "vietnam-ai-travel",
-    "storageBucket": "vietnam-ai-travel.appspot.com",
-    "messagingSenderId": "725014092380",
-    "appId": "1:725014092380:web:c2920e5a7ded9a747f2499",
-    "measurementId": "G-JLCFEFDPCG"
+    "apiKey": os.getenv("FIREBASE_API_KEY"),
+    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL": os.getenv("FIREBASE_DB_URL"),
+    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("FIREBASE_MSG_SENDER_ID"),
+    "appId": os.getenv("FIREBASE_APP_ID"),
+    "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID"),
 }
 
 firebase = pyrebase.initialize_app(firebase_config)
