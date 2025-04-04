d3244f63f006c134203a9135134bdd961f26aa2d
Victor Chuyen <167945697+tncsharetool@users.noreply.github.com>
2025-04-04 20:15:02 +0700

    Update user_avatar.py

--- core/ui/user_avatar.py
+++ core/ui/user_avatar.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def show_user_avatar():
-    avatar_url = "https://i.imgur.com/3G3QZ5H.png"
+    avatar_url = "https://imgur.com/a/fAPDnqF"
     st.sidebar.image(avatar_url, width=80, caption="Guest User")
