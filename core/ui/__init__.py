0cb53af4ca2ff7df5770b603911593868d869708
Victor Chuyen <167945697+tncsharetool@users.noreply.github.com>
2025-04-04 19:45:19 +0700

    Create __init__.py

---
+++ core/ui/__init__.py
@@ -0,0 +1,6 @@
+from .language_switcher import language_switcher
+from .theme_switcher import theme_switcher
+from .footer import show_footer
+from .user_avatar import show_user_avatar
+from .prompt_suggestions import show_prompt_suggestions
+from .trending_prompts import show_trending_prompts
