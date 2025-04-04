091ff6051ed47bd012553dc495d5091d87518223
Victor Chuyen <167945697+tncsharetool@users.noreply.github.com>
2025-04-04 19:44:40 +0700

    Update language_switcher.py

--- core/ui/language_switcher.py
+++ core/ui/language_switcher.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
 def language_switcher():
-    col1, col2 = st.columns([0.1, 0.9])
-    with col1:
-        st.write("🌐")
-    with col2:
-        language = st.selectbox("", ["vi", "en"], index=0, label_visibility="collapsed")
-    st.session_state["lang"] = language
-    return language
+    lang = st.sidebar.selectbox("🌍 Language", ["vi", "en"], index=0)
+    return lang
