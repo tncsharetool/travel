90e89ce05ab5960aacf5568ee2984badcf862b62
Victor Chuyen <167945697+tncsharetool@users.noreply.github.com>
2025-04-04 19:43:39 +0700

    Update user_avatar.py

--- core/ui/user_avatar.py
+++ core/ui/user_avatar.py
@@ -1,13 +1,5 @@
-# core/ui/user_avatar.py
 import streamlit as st
 
-def user_avatar(user_info=None):
-    with st.sidebar:
-        st.markdown("### 👤 User Info")
-        if user_info and "avatar_url" in user_info:
-            st.image(user_info["avatar_url"], width=80)
-        else:
-            st.image("https://www.gravatar.com/avatar/?d=mp", width=80)
-
-        st.write(user_info.get("name", "Guest"))
-        st.caption(user_info.get("email", "Not logged in"))
+def show_user_avatar():
+    avatar_url = "https://i.imgur.com/3G3QZ5H.png"
+    st.sidebar.image(avatar_url, width=80, caption="Guest User")
